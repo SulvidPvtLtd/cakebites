5ed0dcfc839fa0b50ab2e9562dd07026135a260e
Sulvid <chikungurud@hotmail.com>
2026-02-28 22:15:12 +0200

    separeated order and product subcsription into their own files

--- src/app/(admin)/orders/list/subscriptions.ts
+++ src/app/(admin)/orders/list/subscriptions.ts
@@ -1,1 +1,24 @@
-export {};
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { supabase } from "@/src/lib/supabase";
+
+export const useInsertOrderSubscription = () => {
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const orders = supabase
+      .channel("custom-insert-channel")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "orders" },
+        () => {
+          queryClient.invalidateQueries({ queryKey: ["orders"] });
+        }
+      )
+      .subscribe();
+
+    return () => {
+      orders.unsubscribe();
+    };
+  }, [queryClient]);
+};
